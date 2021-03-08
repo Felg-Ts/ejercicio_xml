5f5ca23092c8f3189968e5aed37d683b7ad97f64
Felg-Ts <el-feli-rubio1996@hotmail.com>
2021-03-08 18:03:45 +0100

    Update funciones.py
    Añado al fichero de funciones la tercera función.

--- funciones.py
+++ funciones.py
@@ -33,3 +33,51 @@ def countinfo(root):
     print("Presione una tecla para continuar...")
     msvcrt.getch()
     os.system('clear')
+
+def searhinfo(root):
+
+    print('''
+    1-FIFA
+    2-COD
+    3-FORTNITE
+    4-LOL
+    5-OVERWATCH
+    6-GTA
+    ''')
+
+    ngame = input("Introduce el nombre de un juego: ")
+
+    if ngame == 'FIFA':
+        NG1 = root.find("./tiendas/videojuegos[1]/nombreVideojuego")
+        DG2 = root.find("./tiendas/videojuegos[1]/fechaLanzamiento")
+        PG3 = root.find("./tiendas/videojuegos[1]/precioInicial")
+        print(f'El videojuego {NG1.text} se lanzo el {DG2.text} y tenia un coste inicial de {PG3.text}.')
+    elif ngame == 'COD':
+        NG1 = root.find("./tiendas/videojuegos[4]/nombreVideojuego")
+        DG2 = root.find("./tiendas/videojuegos[4]/fechaLanzamiento")
+        PG3 = root.find("./tiendas/videojuegos[4]/precioInicial")
+        print(f'El videojuego {NG1.text} se lanzo el {DG2.text} y tenia un coste inicial de {PG3.text}.')
+    elif ngame == 'FORTNITE':
+        NG1 = root.find("./tiendas/videojuegos[5]/nombreVideojuego")
+        DG2 = root.find("./tiendas/videojuegos[5]/fechaLanzamiento")
+        PG3 = root.find("./tiendas/videojuegos[5]/precioInicial")
+        print(f'El videojuego {NG1.text} se lanzo el {DG2.text} y tenia un coste inicial de {PG3.text}.')
+    elif ngame == 'LOL':
+        NG1 = root.find("./tiendas/videojuegos[6]/nombreVideojuego")
+        DG2 = root.find("./tiendas/videojuegos[6]/fechaLanzamiento")
+        PG3 = root.find("./tiendas/videojuegos[6]/precioInicial")
+        print(f'El videojuego {NG1.text} se lanzo el {DG2.text} y tenia un coste inicial de {PG3.text}.')
+    elif ngame == 'OVERWATCH':
+        NG1 = root.find("./tiendas/videojuegos[7]/nombreVideojuego")
+        DG2 = root.find("./tiendas/videojuegos[7]/fechaLanzamiento")
+        PG3 = root.find("./tiendas/videojuegos[7]/precioInicial")
+        print(f'El videojuego {NG1.text} se lanzo el {DG2.text} y tenia un coste inicial de {PG3.text}.')
+    elif ngame == 'GTA':
+        NG1 = root.find("./tiendas/videojuegos[8]/nombreVideojuego")
+        DG2 = root.find("./tiendas/videojuegos[8]/fechaLanzamiento")
+        PG3 = root.find("./tiendas/videojuegos[8]/precioInicial")
+        print(f'El videojuego {NG1.text} se lanzo el {DG2.text} y tenia un coste inicial de {PG3.text}.')
+
+    print("Presione una tecla para continuar...")
+    msvcrt.getch()
+    os.system('clear')
